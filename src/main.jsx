8244fe9730a02a3a99db3b5bf29c5e7a708a4529
Fares-sghaier# <faroussassg007@gmail.com>
2022-09-22 11:16:28 +0100

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from './App'
 import './index.css'
 import "./style/flexboxgrid.min.css";
@@ -9,7 +9,7 @@ import "slick-carousel/slick/slick-theme.css";
 import "./style/flexboxgrid.min.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 )
